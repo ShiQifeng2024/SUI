b581a9cef60509fbcf7b29eeb7f3c4b4f7212b73
shiqifeng0473 <shiqifeng0473@yundasys.com>
2023-07-02 13:14:18 +0800

    first commit

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    singleQuote: true, //字符串是否使用单引号，默认为false，使用双引号
+    endOfLine: 'auto', //避免报错delete (cr)的错
+    semi: false,
+    arrowParens:'avoid',
+    trailingComma: 'none', // 结尾处不加逗号
+    htmlWhitespaceSensitivity: 'ignore', // 忽略'>'下落问题
+  }
+  
+  
